fbbff6927549aab05f78abc102b4df3e4e057518
SanphetLovesTina <sanphetlovestina@SanphetLovesTinas-MacBook-Air.local>
2023-11-15 19:08:11 +0700

    Add about me in the form of code fixed number

--- app/components/app/AboutMe.tsx
+++ app/components/app/AboutMe.tsx
@@ -12,14 +12,14 @@ const AboutMe = () => {
       <pre data-prefix="4"><code><span className='text-blue-300'>{`    this`}</span>.age = {age};</code></pre>
       <pre data-prefix="5"><code><span className='text-blue-300'>{`    this`}</span>.sports = ['armwrestling','playing the guitar'];</code></pre>
       <pre data-prefix="6" className='text-warning'><code>{`  }`}</code></pre> 
-      <pre data-prefix="6" className='text-warning'><code>{`  `}</code></pre> 
-      <pre data-prefix="6" className='text-success'><code>{`  speak(){`}</code></pre> 
-      <pre data-prefix="6"><code>{`    console.log('English')`}</code></pre> 
-      <pre data-prefix="6"><code>{`    console.log('Thai')`}</code></pre> 
-      <pre data-prefix="6"><code>{`    console.log('Spanish')`}</code></pre> 
-      <pre data-prefix="6"><code>{`    console.log('Chinese')`}</code></pre> 
-      <pre data-prefix="6" className='text-success'><code>{`  }`}</code></pre> 
-      <pre data-prefix="7" className='text-success'><code>{`}`}</code></pre>
+      <pre data-prefix="7" className='text-warning'><code>{`  `}</code></pre> 
+      <pre data-prefix="8" className='text-success'><code>{`  speak(){`}</code></pre> 
+      <pre data-prefix="9"><code>{`    console.log('English')`}</code></pre> 
+      <pre data-prefix="10"><code>{`    console.log('Thai')`}</code></pre> 
+      <pre data-prefix="11"><code>{`    console.log('Spanish')`}</code></pre> 
+      <pre data-prefix="12"><code>{`    console.log('Chinese')`}</code></pre> 
+      <pre data-prefix="13" className='text-success'><code>{`  }`}</code></pre> 
+      <pre data-prefix="14" className='text-success'><code>{`}`}</code></pre>
     </div>
     </section>
   )
