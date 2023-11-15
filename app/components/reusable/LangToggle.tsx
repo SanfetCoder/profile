dac747f84ffba6fdf0cee1cc46abf41ae1557e71
SanphetLovesTina <sanphetlovestina@SanphetLovesTinas-MacBook-Air.local>
2023-11-15 16:40:07 +0700

    Fix toggle button animation

--- app/components/reusable/LangToggle.tsx
+++ app/components/reusable/LangToggle.tsx
@@ -1,23 +1,25 @@
+import { transform } from 'next/dist/build/swc';
 import React from 'react'
 
 const LangToggle : React.FC<{
   value : string,
-  options : string[],
   setValue : (value : string) => void
 }>
- = ({value, options, setValue}) => {
+ = ({value, setValue}) => {
 
   const handleToggle = () => {
-    if (value === options[0]) setValue(options[1]);
-    if (value === options[1]) setValue(options[0]);
+    if (value === 'th') setValue('eng');
+    if (value === 'eng') setValue('th');
   }
 
   return (
     <button onClick={handleToggle} className=" relative border-solid border-2 border-red-400 w-[6rem] h-[3rem] rounded-full">
       <div className='flex flex-row items-center justify-between px-1.5'>
-        {value === options[0] && <p className='w-1/2 absolute'>{value}</p>}
-        <span className={`flex translate-x-[${value === options[0] ? '45' : '0'}px] justify-center items-center ${value === options[0] ? 'bg-[url("https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png")]' : 'bg-[url("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_Thailand.svg/255px-Flag_of_Thailand.svg.png")]'} bg-cover w-[2rem] h-[2rem] rounded-full transition-all`}></span>
-        {value === options[1] && <p className='w-1/2 absolute right-0'>{value}</p>}
+        {value === 'eng' && <p className='w-1/2 absolute'>{value}</p>}
+        <span
+         style={{transform : `translateX(${value === 'eng' ? '45' : '0'}px)`}}
+         className={`flex justify-center items-center ${value === 'eng' ? 'bg-[url("https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png")]' : 'bg-[url("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_Thailand.svg/255px-Flag_of_Thailand.svg.png")]'} bg-cover w-[2rem] h-[2rem] rounded-full transition-all`}></span>
+        {value === 'th' && <p className='w-1/2 absolute right-0'>{value}</p>}
       </div>
     </button>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import LangToggle from "./components/reusable/LangToggle";
 import styles from './app.module.css';
 
 export default function Home() {
-  const [language, setLanguage] = useState('en');
+  const [language, setLanguage] = useState('eng');
 
   return (
     <main className="bg-[#080716] h-screen w-screen px-24 py-10">
       <NavBar>
         <Logo />
-        <LangToggle setValue={setLanguage} options={['en','th']} value={language}/>
+        <LangToggle setValue={setLanguage} value={language}/>
       </NavBar>
       <Hero language={language}/>
     </main>
@@ -43,15 +43,15 @@ const Hero : React.FC<{
   return (
     <div className="flex flex-row text-white mt-[7rem]">
       <div className="flex flex-col justify-center w-2/3">
-        {language === 'en' ?
+        {language === 'eng' ?
           <h1 className="text-4xl">Hi there <span className={styles.blogTitleEmoji}>👋</span>, I am <span className="text-red-400 font-semibold">Sanphet Songjindasak</span></h1>
         :
           <h1 className="text-4xl">สวัสดีครับ <span className={styles.blogTitleEmoji}>👋</span>, ผมชื่อ <span className="text-red-400 font-semibold">สรรเพชญสงค์จินดาศักดิ์</span></h1>
         }
         <h1 className="underline decoration-red-400 decoration-5 text-4xl mt-5">Full-stack developer</h1>
         <div className="flex flex-row gap-x-10 mt-10">
-          <button className="btn bg-red-400 border-none text-white text-lg w-[10rem] h-[4rem] rounded-full hover:scale-110 hover:bg-red-400 transition-all">{language === 'en' ? 'Hire me' : 'ว่าจ้าง'}</button>
-          <button className="btn bg-gray-200 text-black rounded-full w-[10rem] h-[4rem] text-lg hover:scale-110 transition-all">{language === 'en' ? 'My works' : 'ผลงาน'}</button>
+          <button className="btn bg-red-400 border-none text-white text-lg w-[10rem] h-[4rem] rounded-full hover:scale-110 hover:bg-red-400 transition-all">{language === 'eng' ? 'Hire me' : 'ว่าจ้าง'}</button>
+          <button className="btn bg-gray-200 text-black rounded-full w-[10rem] h-[4rem] text-lg hover:scale-110 transition-all">{language === 'eng' ? 'My works' : 'ผลงาน'}</button>
         </div> 
       </div>
       <img className="w-1/3 rounded-full p-2 bg-red-400 h-1/2 bg-contain" src="https://avatars.githubusercontent.com/u/118830176?v=4" alt="" />
