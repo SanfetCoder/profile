d61be4423c290a1f716a6488940fb4f3a681b5e9
Yang Predator <newkidsth@gmail.com>
2024-03-17 23:52:35 +0700

    Add Footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full h-[80px] bg-gray-900 m-0 box-border'>
+        
+    </footer>
+  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,43 @@
-'use client';
-import { ReactComponentElement, ReactNode, useState } from "react"
+"use client";
+import { ReactComponentElement, ReactNode, useState } from "react";
 import LangToggle from "./components/reusable/LangToggle";
-import styles from './app.module.css';
+import styles from "./app.module.css";
 import { TechStack } from "./components/app/TechStack";
 import AboutMe from "./components/app/AboutMe";
 import Image from "next/image";
 import MyWorks from "./components/app/MyWorks";
+import Footer from "./components/Footer";
 
 export default function Home() {
-  const [language, setLanguage] = useState('eng');
-  const currentFont = language === 'eng' ? 'poppins' : 'prompt';
+  const [language, setLanguage] = useState("eng");
+  const currentFont = language === "eng" ? "poppins" : "prompt";
 
   return (
-    <main className={`bg-[#080716] h-screen w-screen px-24 py-10 overflow-y-scroll font-${currentFont} scroll-smooth`}>
-      <NavBar>
-        <Logo />
-        <LangToggle setValue={setLanguage} value={language}/>
-      </NavBar>
-      <Hero language={language}/>
-      <TechStack language={language}/>
-      <AboutMe language={language}/>
-      <MyWorks language={language}/>
+    <main
+      className={`bg-[#080716] flex flex-col h-full w-full overflow-y-hidden overflow-x-hidden font-${currentFont} scroll-smooth`}
+    >
+      <div className="px-24 py-10 w-full h-full">
+        <NavBar>
+          <Logo />
+          <LangToggle setValue={setLanguage} value={language} />
+        </NavBar>
+        <Hero language={language} />
+        <TechStack language={language} />
+        <AboutMe language={language} />
+        <MyWorks language={language} />
+      </div>
+      <Footer />
     </main>
-  )
+  );
 }
 
-const NavBar : React.FC<{
-  children : ReactNode
-}> = ({children}) => {
+const NavBar: React.FC<{
+  children: ReactNode;
+}> = ({ children }) => {
   return (
-    <nav className="flex flex-row justify-between text-white ">
-      {children}
-    </nav>
-  )
-}
+    <nav className="flex flex-row justify-between text-white ">{children}</nav>
+  );
+};
 
 const Logo = () => {
   return (
@@ -41,33 +45,61 @@ const Logo = () => {
       <h1 className="text-3xl font-semibold">Sanfet Coder</h1>
       <span className="bg-red-300 w-[10px] h-[10px] absolute rounded-full right-0 bottom-3 translate-x-4"></span>
     </div>
+  );
+};
 
-  )
-}
-
-const Hero : React.FC<{
-  language : string
-}> = ({language}) => {
-
+const Hero: React.FC<{
+  language: string;
+}> = ({ language }) => {
   const navigateToFastwork = () => {
     window.location.assign("https://fastwork.co/user/sanfet");
-  }
+  };
 
   return (
     <div className="flex flex-row text-white mt-[7rem]">
       <div className="flex flex-col justify-center w-2/3">
-        {language === 'eng' ?
-          <h1 className="text-4xl">Hi there <span className={styles.blogTitleEmoji}>👋</span>, I am <span className="text-red-400 font-semibold">Sanphet Songjindasak</span></h1>
-        :
-          <h1 className="text-4xl font-prompt">สวัสดีครับ <span className={styles.blogTitleEmoji}>👋</span>, ผมชื่อ <span className="text-red-400 font-semibold font-prompt">สรรเพชญ สงค์จินดาศักดิ์</span></h1>
-        }
-        <h1 className={`underline decoration-red-400 decoration-5 text-4xl mt-5 ${styles.typingContainer}`}>Full-stack developer</h1>
+        {language === "eng" ? (
+          <h1 className="text-4xl">
+            Hi there <span className={styles.blogTitleEmoji}>👋</span>, I am{" "}
+            <span className="text-red-400 font-semibold">
+              Sanphet Songjindasak
+            </span>
+          </h1>
+        ) : (
+          <h1 className="text-4xl font-prompt">
+            สวัสดีครับ <span className={styles.blogTitleEmoji}>👋</span>, ผมชื่อ{" "}
+            <span className="text-red-400 font-semibold font-prompt">
+              สรรเพชญ สงค์จินดาศักดิ์
+            </span>
+          </h1>
+        )}
+        <h1
+          className={`underline decoration-red-400 decoration-5 text-4xl mt-5 ${styles.typingContainer}`}
+        >
+          Full-stack developer
+        </h1>
         <div className="flex flex-row gap-x-10 mt-10">
-          <button onClick={navigateToFastwork} className="btn bg-red-400 border-none text-white text-lg w-[10rem] h-[4rem] rounded-full hover:scale-110 hover:bg-red-400 transition-all">{language === 'eng' ? 'Hire me' : 'ว่าจ้าง'}</button>
-          <a href="#myWork" className="btn bg-gray-200 text-black rounded-full w-[10rem] h-[4rem] text-lg hover:scale-110 transition-all">{language === 'eng' ? 'My works' : 'ผลงาน'}</a>
-        </div> 
+          <button
+            onClick={navigateToFastwork}
+            className="btn bg-red-400 border-none text-white text-lg w-[10rem] h-[4rem] rounded-full hover:scale-110 hover:bg-red-400 transition-all"
+          >
+            {language === "eng" ? "Hire me" : "ว่าจ้าง"}
+          </button>
+          <a
+            href="#myWork"
+            className="btn bg-gray-200 text-black rounded-full w-[10rem] h-[4rem] text-lg hover:scale-110 transition-all"
+          >
+            {language === "eng" ? "My works" : "ผลงาน"}
+          </a>
+        </div>
       </div>
-      <Image width={300} height={300} className={`w-1/3 rounded-full p-2 bg-red-400 h-1/2 bg-contain ${styles.animationFloating}`} src="/main/img-profile.jpeg" alt="" />
+      <Image
+        width={300}
+        height={300}
+        className={`w-1/3 rounded-full p-2 bg-red-400 h-1/2 bg-contain ${styles.animationFloating}`}
+        src="/main/img-profile.jpeg"
+        alt=""
+      />
     </div>
-  )
-}
+  );
+};
